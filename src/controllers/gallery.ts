d040e28ee7b7f860902ee96e056f4ce8cfd73ae9
Ikhtiyor <an.ikhtiyour@gmail.com>
2021-11-15 15:10:39 +0500

    file changed

--- src/controllers/gallery.ts
+++ src/controllers/gallery.ts
@@ -16,17 +16,6 @@ export class GalleryController{
         })
     })
 
-    get = catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
-        const gallery = await storage.gallery.findById(req.params.id)
-
-        res.status(200).json({
-            success:true,
-            data:{
-                gallery
-            }
-        })
-    })
-
     getOne = catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
         const gallery = await storage.gallery.findOne(req.body)
 
@@ -39,20 +28,9 @@ export class GalleryController{
     })
 
     create = catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
-        const category = await storage.category.create({...req.body, photo:req.file?.filename})
+        const gallery = await storage.gallery.create({...req.body, photo:req.file?.filename})
 
         res.status(201).json({
-            success:true,
-            data:{
-                category
-            }
-        })
-    })
-
-    update = catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
-        const gallery = await storage.gallery.update(req.params.id,req.body)
-
-        res.status(200).json({
             success:true,
             data:{
                 gallery
